8287c5d734bb52ae3d9d5681a8fd9497ca965b37
Mahmoud Alqudah <mahmoudodeh332@gmail.com>
2025-02-18 10:37:52 +0300

    Yes

--- libraryproject/apps/bookmodule/views.py
+++ libraryproject/apps/bookmodule/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request): 
-    name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html",{'name':name})
-
-
-def index2(request, val1 = 0):   #add the view function (index2)
-    return HttpResponse("value1 = "+str(val1))
-
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
 
 def viewbook(request, bookId):
     # assume that we have the following books somewhere (e.g. database)
