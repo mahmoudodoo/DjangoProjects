9492eafc8417d8b7bc8601a5e30a264f237d5f12
Mahmoud Alqudah <mahmoudodeh332@gmail.com>
2025-02-23 16:35:36 +0300

    Tasd

--- libraryproject/apps/bookmodule/urls.py
+++ libraryproject/apps/bookmodule/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('list_books/', views.list_books, name= "books.list_books"),
     path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
     path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('html5/links/', views.links_page, name='books.links'),
+    path('html5/text/formatting/', views.text_formatting_page, name='books.text_formatting'),
+    path('html5/listing/', views.listing_page, name='books.listing'),
+    path('html5/tables/', views.tables_page, name='books.tables'),
 ]
--- libraryproject/apps/bookmodule/views.py
+++ libraryproject/apps/bookmodule/views.py
@@ -15,12 +15,15 @@ def viewbook(request, bookId):
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
 
-def viewbook(request, bookId):
-    # assume that we have the following books somewhere (e.g. database)
-    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
-    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
-    targetBook = None
-    if book1['id'] == bookId: targetBook = book1
-    if book2['id'] == bookId: targetBook = book2
-    context = {'book':targetBook} # book is the variable name accessible by the template
-    return render(request, 'bookmodule/show.html', context)
+def links_page(request):
+    return render(request, 'bookmodule/links.html')
+
+def text_formatting_page(request):
+    return render(request, 'bookmodule/text_formatting.html')
+
+
+def listing_page(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables_page(request):
+    return render(request, 'bookmodule/tables.html')
